ce2c5f1de057ee967aa7c6f272d8bfbaab892bf9
Curia <GheorgheAdrian.Curia@ihs.com>
2018-05-22 15:04:16 +0300

    10 - Controllers - json Action Result; serializer object to json

--- OdeToFood/Controllers/HomeController.cs
+++ OdeToFood/Controllers/HomeController.cs
@@ -1,15 +1,25 @@
-﻿using System;
+﻿using Microsoft.AspNetCore.Mvc;
+using OdeToFood.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace OdeToFood.Controllers
 {
-    public class HomeController
+    public class HomeController : Controller
     {
-        public string Index()
+        public IActionResult Index()
         {
-            return "Hello from HomeController!";
+            //this.ControllerContext.ActionDescriptor.ActionName;
+            //this.HttpContext.Response.OnStarting.ToString; // do not use this offen
+
+            //return this.BadRequest();
+            //return Content("Hello from HomeController!"); // this is where we decide what to return; Because of IActionResolut, the MVC framework will be the one to execute this and do return the result to the client
+
+            var model = new Restaurant { Id = 1, Name= "Pizzeria Tome"};
+
+            return new ObjectResult(model);
         }
     }
 }
