a6a1112216db8fcfa3fd00aa2b86214c506eaac2
Curia <GheorgheAdrian.Curia@ihs.com>
2018-05-22 14:23:32 +0300

    9 - Controllers - Attribute Routes

---
+++ OdeToFood/Controllers/ServiceController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace OdeToFood.Controllers
+{
+    // /about for phone action
+    [Route("[controller]")]
+    //[Route("company/[controller]/[action]")]
+    public class ServiceController
+    {
+        [Route("")]
+        public string Phone()
+        {
+            return "1223123 AboutController!";
+        }
+
+        [Route("[action]")]
+        public string Address()
+        {
+            return "USA address AboutController!";
+        }
+    }
+}
