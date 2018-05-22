2df11b73c5e99b5e9b811b72e0e068079ee1a429
Curia <GheorgheAdrian.Curia@ihs.com>
2018-05-22 11:57:19 +0300

    middleware execution order and Low Level Middleware app.User

--- OdeToFood/Startup.cs
+++ OdeToFood/Startup.cs
@@ -7,6 +7,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
 
 namespace OdeToFood
 {
@@ -23,18 +24,49 @@ namespace OdeToFood
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, 
                                 IHostingEnvironment env,
-                                IGreeter greeter)
+                                IGreeter greeter,
+                                ILogger<Startup> logger)
         {
-            if (env.IsDevelopment())
+            //if (env.IsDevelopment())
+            //{
+            //    app.UseDeveloperExceptionPage();
+            //}
+
+            // delegates and the func type
+            // app.Use - write lower level middleware
+
+            app.Use(next => //allow next piece of middlevare to process this request
             {
-                app.UseDeveloperExceptionPage();
-            }
+                return async context => //this is inwoked onece per http request
+                {
+                    logger.LogInformation("Request incoming!");
+                    if (context.Request.Path.StartsWithSegments("/mym"))
+                    {
+                        await context.Response.WriteAsync("Hit!");
+                        logger.LogInformation("Request handled!");
+                    }
+                    else // let the next middleware chance to respond
+                    {
+                        await next(context);
+                        logger.LogInformation("Response outgoind!"); // this is the control flow back of the pipeline
+                    }
+                };
+            });
+
+            app.UseWelcomePage(new WelcomePageOptions
+            {
+                Path = "/wp"
+            });
+
             // this runs for every request that we receive
             app.Run(async (context) =>
             {
                 var customGreeting = greeter.GetMessageOfTheDay();
                 await context.Response.WriteAsync(customGreeting);
             });
+
+
         }
     }
 }
+
