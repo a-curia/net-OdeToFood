cb72a174e843d898d706418ad0c861ee80b1faa1
Curia <GheorgheAdrian.Curia@ihs.com>
2018-05-24 12:53:55 +0300

    5-1 Entity Models and View Models(input Models)

--- OdeToFood/Controllers/HomeController.cs
+++ OdeToFood/Controllers/HomeController.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using OdeToFood.Models;
 using OdeToFood.Services;
+using OdeToFood.ViewModels;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -11,10 +12,13 @@ namespace OdeToFood.Controllers
     public class HomeController : Controller
     {
         private IRestaurantData _restaurantData;
+        private IGreeter _greeter;
 
-        public HomeController(IRestaurantData restaurantData)
+        public HomeController(IRestaurantData restaurantData,
+                            IGreeter greeter)
         {
             _restaurantData = restaurantData;
+            _greeter = greeter;
         }
 
         public IActionResult Index()
@@ -26,7 +30,10 @@ namespace OdeToFood.Controllers
             //return Content("Hello from HomeController!"); // this is where we decide what to return; Because of IActionResolut, the MVC framework will be the one to execute this and do return the result to the client
 
             //var model = new Restaurant { Id = 1, Name= "Pizzeria Tome"};
-            var model = _restaurantData.GetAll();
+            //var model = _restaurantData.GetAll();
+            var model = new HomeIndexViewModel();
+            model.Restaurants = _restaurantData.GetAll();
+            model.CurrentMessage = _greeter.GetMessageOfTheDay();
 
             return View(model);
         }
