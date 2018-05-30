d0430393059073451aeef3aeea80e8718e6694d2
Curia <GheorgheAdrian.Curia@ihs.com>
2018-05-30 14:52:29 +0300

    6-4 Implementing a DbContext

---
+++ OdeToFood/Services/SqlRestaurantData.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using OdeToFood.Data;
+using OdeToFood.Models;
+
+namespace OdeToFood.Services
+{
+    public class SqlRestaurantData : IRestaurantData
+    {
+        private OdeToFoodDbContext _context;
+
+        public SqlRestaurantData(OdeToFoodDbContext context)
+        {
+            _context = context;
+        }
+
+        public Restaurant AddNewRestaurant(Restaurant newRestaurant)
+        {
+            _context.Restaurants.Add(newRestaurant);
+            _context.SaveChanges();
+
+            return newRestaurant;
+        }
+
+        public IEnumerable<Restaurant> GetAll() // for larger applications use IQueryable instead of IEnumerable which store everything into memory
+        {
+            return _context.Restaurants.OrderBy(r => r.Name);
+        }
+
+        public Restaurant GetById(int id)
+        {
+            return _context.Restaurants.FirstOrDefault(r => r.Id == id);
+        }
+    }
+}
